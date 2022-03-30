0e165228749bce90292f1a05d49c87fd36b41c49
Yousef Samara <yousef.aa.samara@gmail.com>
2022-03-30 23:16:04 +0300

    node

---
+++ NODE/NODE.md
@@ -0,0 +1,10 @@
+# chaling linked list
+
+made a linked list that have a head and nodes and insert data to it.
+
+
+
+![image](node.png)
+
+
+
--- NODE/NODE/Program.cs
+++ NODE/NODE/Program.cs
@@ -7,9 +7,66 @@ namespace NODE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
         }
- 
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int x) // constructor that take new data that i want to put in list
+            {
+                data = x;
+                next = null;
+            }
+        }
+
+
+        public class LinkedList
+        {
+            Node head;
+            public LinkedList()
+            {
+                head = null;
+                // int count = 0;
+            }
+            public void Insert(int data)
+            {
+
+                Node node = new Node(data);
+                node.next = head;
+                head = node;
+                //  count++
+            }
+            //public void PrintList()
+            //{
+            //    Node runner = head;
+            //    while (runner != null)
+            //    {
+            //        Console.WriteLine(runner.data);
+            //    }
+            //}
+
+            public void ToSstring()
+            {
+                Node n = head;
+                if (n == null)
+                {
+                    Console.WriteLine("Your llist is empty");
+                }
+                else
+                {
+                    while (n != null)
+                    {
+                        Console.Write("[" + n.data + "] -> ");
+                        n = n.next;
+                    }
+                    Console.WriteLine("null");
+                }
+            }
+
+
+        }
+
 
     }
 }
